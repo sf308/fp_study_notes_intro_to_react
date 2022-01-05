e45b34e783f6a6a58579a9ac383ade44e1b39f6d
Souheil  Fenghour <souheil@getfutureproof.co.uk>
2022-01-05 15:42:21 +0000

    format Lik button to put it on the same line as the song

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -20,13 +20,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Franz_Ferdinand_-_O2_Academy_Brixton_-_Saturday_24th_February_2018_FranzFBrixton240218-38_%2826699814248%29.jpg/450px-Franz_Ferdinand_-_O2_Academy_Brixton_-_Saturday_24th_February_2018_FranzFBrixton240218-38_%2826699814248%29.jpg" alt="logo" />
-        <h1>
-          Franz Ferdinand
-        </h1>
-        <p>Genre: Indie rock</p>
-        <p role="like"> Likes: {likes}</p> <button role="likebutton" onClick={() => updateLikes(likes, setLikes)}>+</button>
+        <div id="band-title">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Franz_Ferdinand_-_O2_Academy_Brixton_-_Saturday_24th_February_2018_FranzFBrixton240218-38_%2826699814248%29.jpg/450px-Franz_Ferdinand_-_O2_Academy_Brixton_-_Saturday_24th_February_2018_FranzFBrixton240218-38_%2826699814248%29.jpg" alt="logo" />
+          <h1 id="band">
+            Franz Ferdinand
+          </h1>
+          <p>Genre: Indie rock</p>
+          <p role="like"> Likes: {likes}</p> <button role="likebutton" onClick={() => updateLikes(likes, setLikes)}>+</button>
+        </div>
+        <div id="band-intro">
         <p>Franz Ferdinand are a Scottish rock band formed in Glasgow in 2002. The band's original line-up was composed of Alex Kapranos (lead vocals, lead guitar, keyboards), Nick McCarthy (rhythm guitar, keyboards, backing vocals), Bob Hardy (bass guitar), and Paul Thomson (drums, percussion, backing vocals). Julian Corrie (keyboards, lead guitar, backing vocals) and Dino Bardot (rhythm guitar, backing vocals) joined the band in 2017 after McCarthy left during the previous year, and Audrey Tait (drums) joined the band after Thomson left in 2021. The band is one of the more popular post-punk revival bands, garnering multiple UK top 20 hits.[1] They have been nominated for several Grammy Awards and have received two Brit Awards – winning one for Best British Group – as well as one NME Award.</p>
+        </div>
+        <div id="songs">
         <ul>
         <li><a
           className="App-link"
@@ -66,6 +71,7 @@ function App() {
         </a><p> Likes: {s4likes}</p> <button onClick={() => updateLikes(s4likes, s4setLikes)}>+</button>
         </li>
         </ul>
+        </div>
       </header>
     </div>
   );
